369369da12349f4a6f7ca9ffc8eeaec2a8b7ffc8
DLohmann <DLohmann@ucmerced.edu>
2018-04-16 03:08:17 -0700

    first commit of a project I did a fw years ago

---
+++ Contact List/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace Contact_List
+{
+    partial class ContactForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddButton = new System.Windows.Forms.Button();
+            this.PersonTextBox = new System.Windows.Forms.TextBox();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.Person = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Phone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.PhoneTextBox = new System.Windows.Forms.TextBox();
+            this.PersonLabel = new System.Windows.Forms.Label();
+            this.PhoneLabel = new System.Windows.Forms.Label();
+            this.SaveButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(12, 33);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(119, 23);
+            this.AddButton.TabIndex = 1;
+            this.AddButton.Text = "Add";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PersonTextBox
+            // 
+            this.PersonTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PersonTextBox.Location = new System.Drawing.Point(60, 65);
+            this.PersonTextBox.Name = "PersonTextBox";
+            this.PersonTextBox.Size = new System.Drawing.Size(202, 20);
+            this.PersonTextBox.TabIndex = 2;
+            this.PersonTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Person,
+            this.Phone});
+            this.dataGridView.Location = new System.Drawing.Point(12, 117);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.Size = new System.Drawing.Size(250, 256);
+            this.dataGridView.TabIndex = 3;
+            this.dataGridView.RowEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_RowEnter);
+            // 
+            // Person
+            // 
+            this.Person.HeaderText = "Person";
+            this.Person.Name = "Person";
+            // 
+            // Phone
+            // 
+            this.Phone.HeaderText = "Phone";
+            this.Phone.Name = "Phone";
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.DeleteButton.Location = new System.Drawing.Point(148, 33);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(114, 23);
+            this.DeleteButton.TabIndex = 4;
+            this.DeleteButton.Text = "Delete Selected";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // PhoneTextBox
+            // 
+            this.PhoneTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PhoneTextBox.Location = new System.Drawing.Point(60, 91);
+            this.PhoneTextBox.Name = "PhoneTextBox";
+            this.PhoneTextBox.Size = new System.Drawing.Size(202, 20);
+            this.PhoneTextBox.TabIndex = 5;
+            this.PhoneTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // PersonLabel
+            // 
+            this.PersonLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.PersonLabel.AutoSize = true;
+            this.PersonLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PersonLabel.Location = new System.Drawing.Point(9, 68);
+            this.PersonLabel.Name = "PersonLabel";
+            this.PersonLabel.Size = new System.Drawing.Size(50, 13);
+            this.PersonLabel.TabIndex = 6;
+            this.PersonLabel.Text = "Person:";
+            // 
+            // PhoneLabel
+            // 
+            this.PhoneLabel.AutoSize = true;
+            this.PhoneLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PhoneLabel.Location = new System.Drawing.Point(9, 94);
+            this.PhoneLabel.Name = "PhoneLabel";
+            this.PhoneLabel.Size = new System.Drawing.Size(47, 13);
+            this.PhoneLabel.TabIndex = 7;
+            this.PhoneLabel.Text = "Phone:";
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.SaveButton.Location = new System.Drawing.Point(13, 4);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveButton.TabIndex = 8;
+            this.SaveButton.Text = "Save";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // ContactForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 385);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.PhoneLabel);
+            this.Controls.Add(this.PersonLabel);
+            this.Controls.Add(this.PhoneTextBox);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.PersonTextBox);
+            this.Controls.Add(this.AddButton);
+            this.Name = "ContactForm";
+            this.Text = "Contacts";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.TextBox PersonTextBox;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Person;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Phone;
+        private System.Windows.Forms.TextBox PhoneTextBox;
+        private System.Windows.Forms.Label PersonLabel;
+        private System.Windows.Forms.Label PhoneLabel;
+        private System.Windows.Forms.Button SaveButton;
+    }
+}
+
---
+++ Contact List/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+
+namespace Contact_List
+{
+    
+    public partial class ContactForm : Form
+    {
+        private bool newDoc = false;
+        private XmlTextReader doc;
+        private String filePathName = "../../../XMLSampleNames.xml";
+        public ContactForm()
+        {
+            InitializeComponent();
+            
+
+            try {
+                doc = new XmlTextReader(filePathName);
+                short i = 0;
+                DataGridViewCell selectedCell = null;
+                dataGridView.Rows.Add();
+                while (doc.Read())
+                {
+                    switch (doc.NodeType)
+                    {
+                        case XmlNodeType.Element: // The node is an element.
+                            if (doc.Name.Equals("PersonName"))
+                            {
+                                dataGridView.Rows.Add();
+                                selectedCell = dataGridView.Rows[i].Cells[0];
+                            }
+                            else if (doc.Name.Equals("phone"))
+                            {
+                                selectedCell = dataGridView.Rows[i].Cells[1];
+                            }
+                            break;
+                        case XmlNodeType.Text: //Display the text in each element.
+                            selectedCell.Value = doc.Value;
+                            break;
+                        case XmlNodeType.EndElement: //Display the end of the element.
+                            if (doc.Name.Equals("phone"))
+                            {
+                                i++;
+                            }
+                            break;
+                    }
+                }
+                
+                
+                // put the data from this text file intogrid view
+               /* String line;
+                short i = 0;
+                line = doc.ReadLine();
+                if (line == null)
+                {
+                    MessageBox.Show("The test file is empty");
+                }
+                else
+                {
+                    while (line != null)
+                    {
+                        MessageBox.Show(line);
+                        // THERE IS AN ERROR HERE
+                        dataGridView.Rows[i].Cells[0].Value = line; // this populates the "name column with the lines of file
+
+
+
+                        line = doc.ReadLine();
+                        i++;
+                    }
+                }
+                */
+                    doc.Close();
+            } catch (System.IO.FileNotFoundException e) {
+                newDoc = true;
+            }
+
+            
+        }
+        private int lastIndex = -1;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            addToList();
+        }
+
+        private void addToList()
+        {
+            lastIndex++;
+            dataGridView.Rows.Add();
+            dataGridView.Rows[dataGridView.RowCount - 1].Cells[0].Value = PersonTextBox.Text;
+            dataGridView.Rows[dataGridView.RowCount - 1].Cells[1].Value = PhoneTextBox.Text;
+            PersonTextBox.Text = "";
+            PhoneTextBox.Text = "";
+        }
+
+        // add to list
+        // if add button is pressed
+        private void Form1_EnterPress(object sender, System.Windows.Forms.KeyEventArgs e) {
+            addToList();
+        }
+        // if enter is pressed in name box, make user select phone number box
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (e.KeyChar == '\r')// adds to list if "enter" key is pressed
+            {
+                //addToList();
+                PhoneTextBox.Select();
+            }
+            
+        }
+        // if enter is pressed in phone number box, add the person to the list
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == '\r')// adds to list if "enter" key is pressed
+            {
+                addToList();
+                PersonTextBox.Select();
+            }
+        }
+
+        // remove rows
+        private int selectedRow = -1;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Remove selected row (incomplete)
+            if (selectedRow == -1)
+            {
+                MessageBox.Show("Please select the row to delete");
+            } else {
+                dataGridView.Rows.RemoveAt(selectedRow);
+                if (selectedRow == dataGridView.Rows.Count)
+                {
+                    selectedRow --;
+                }
+
+                lastIndex--;
+            }
+            
+            
+        }
+
+        private void dataGridView1_RowEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            selectedRow = e.RowIndex;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //if (newDoc)
+            //{
+                // make a new .txt doc and write the chart on it
+                StreamWriter write = new StreamWriter(filePathName);
+                write.WriteLine("Name\\tPhone");
+                for (short i = 0; i < dataGridView.RowCount; i++)
+                {
+                    write.WriteLine(dataGridView.Rows[i].Cells[0].Value + "\t" + dataGridView.Rows[i].Cells[1].Value);
+                }
+            write.Close();
+            //} else {
+                // write the chart on the old doc
+
+            //}
+           
+        }
+    }
+}
